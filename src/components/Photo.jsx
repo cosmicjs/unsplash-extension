a7d64b455700a80511313a04d5d35792914c87c9
Karl Koch <karl@kejk.tech>
2023-04-26 15:15:51 +0100

    fix: Removes json and zip, improves image aspect ratio

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Loader } from 'semantic-ui-react'
-import { ArrowUpRightIcon } from '@heroicons/react/20/solid'
+import React from "react";
+import { Loader } from "semantic-ui-react";
+import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
 
 function Photo({ url, children, id }) {
   return (
     <>
       <img
         src={`${url}`}
-        className={`object-cover w-full h-96 rounded-2xl overflow-hidden relative z-10`}
+        className={`relative z-10 h-64 w-full overflow-hidden rounded-2xl object-cover`}
       />
-      <div className='text-center z-0 absolute top-64 w-full'>
-        <Loader active inline size='large' />
+      <div className="absolute top-40 z-0 w-full text-center">
+        <Loader active inline size="large" />
       </div>
-      <div className='absolute z-20 text-center w-full bottom-4 items-center justify-center flex space-x-4'>
+      <div className="absolute bottom-4 z-20 flex w-full items-center justify-center space-x-4 text-center">
         {children}
         <a
-          href={'https://unsplash.com/photos/' + id}
-          target='_blank'
-          className='flex items-center justify-center bg-gray-50 border border-gray-300 rounded-md py-2 px-4 w-max hover:bg-gray-200 group-hover:text-gray-700 dark:group-hover:text-gray-400 group-hover:shadow-md group'
+          href={"https://unsplash.com/photos/" + id}
+          target="_blank"
+          className="group flex w-max items-center justify-center rounded-md border border-gray-300 bg-gray-50 px-4 py-2 hover:bg-gray-200 group-hover:text-gray-700 group-hover:shadow-md dark:group-hover:text-gray-400"
         >
-          <span className='mr-2'>Unsplash</span>
+          <span className="mr-2">Unsplash</span>
           <ArrowUpRightIcon
             width={20}
             height={20}
-            className='text-gray-700 dark:text-gray-400 group-hover:text-gray-700 dark:group-hover:text-gray-400 group-hover:translate-x-1 transition-all duration-200 ease-in-out transform'
+            className="transform text-gray-700 transition-all duration-200 ease-in-out group-hover:translate-x-1 group-hover:text-gray-700 dark:text-gray-400 dark:group-hover:text-gray-400"
           />
         </a>
       </div>
     </>
-  )
+  );
 }
 
-export default Photo
+export default Photo;
